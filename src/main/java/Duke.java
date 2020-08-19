22590e2ec58630e2580c71a2de5282380bf45f76
johan9se <johannine.enerio@gmail.com>
2020-08-19 10:40:07 +0800

    Add Echo loop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printGreeting(){
+        System.out.println("\t_________________________________");
+        System.out.println("\t  Hello! I'm Duke");
+        System.out.println("\t  What can I do for you?");
+        System.out.println("\t_________________________________");
+    }
+    public static void printGoodbye(){
+        System.out.println("\t_________________________________");
+        System.out.println("\t  Bye. Hope to see you again soon!");
+        System.out.println("\t_________________________________");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,12 +19,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("―――――――――――――――――――――");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("―――――――――――――――――――――");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("―――――――――――――――――――――");
-
+        printGreeting();
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        while (!line.equals("bye")){
+            System.out.println("\t_________________________________");
+            System.out.println("\t  " + line);
+            System.out.println("\t_________________________________\n");
+            line = in.nextLine();
+        }
+        printGoodbye();
     }
 }
