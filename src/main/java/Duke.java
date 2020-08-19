d2bbdaf52755b64cd9f5979fe9fc5c663616d7ff
johan9se <johannine.enerio@gmail.com>
2020-08-19 12:32:46 +0800

    Add markAsDone in Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,16 +4,20 @@ public class Duke {
     private static Task[] list = new Task[100];
     private static int itemsInList = 0;
 
-    public static void printGreeting(){
-        System.out.println("\t_________________________________");
-        System.out.println("\t  Hello! I'm Duke");
-        System.out.println("\t  What can I do for you?");
+    public static void lineBreak(){
         System.out.println("\t_________________________________");
     }
+    public static void printGreeting(){
+        lineBreak();
+        System.out.println("\t Hello! I'm Duke");
+        System.out.println("\t What can I do for you?");
+        lineBreak();
+    }
+
     public static void printGoodbye(){
-        System.out.println("\t_________________________________");
-        System.out.println("\t  Bye. Hope to see you again soon!");
-        System.out.println("\t_________________________________");
+        lineBreak();
+        System.out.println("\t Bye. Hope to see you again soon!");
+        lineBreak();
     }
 
     public static void addNewListItem(Task item){
@@ -22,17 +26,17 @@ public class Duke {
     }
 
     public static void listItems(){
-        System.out.println("\t_________________________________");
+        lineBreak();
         for (int i=0;i<itemsInList;i++){
-            System.out.printf("%d. [%s] %s\n", i+1, list[i].getStatusIcon(), list[i].description);
+            System.out.printf("\t %d. [%s] %s\n", i+1, list[i].getStatusIcon(), list[i].description);
         }
-        System.out.println("\t_________________________________");
+        lineBreak();
     }
 
     public static void echoItem(String item){
-        System.out.println("\t_________________________________");
-        System.out.println("\t  added: " + item);
-        System.out.println("\t_________________________________");
+        lineBreak();
+        System.out.println("\t added: " + item);
+        lineBreak();
     }
 
     public static void main(String[] args) {
@@ -42,12 +46,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
         printGreeting();
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
         while (!line.equals("bye")){
             if (line.equals("list")){
                 listItems();
+            } else if (line.startsWith("done")) {
+                int taskID = Integer.parseInt((line.split(" "))[1]) - 1;
+                list[taskID].markAsDone();
             } else {
                 Task t = new Task(line);
                 addNewListItem(t);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,4 +10,13 @@ public class Task {
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
+
+    public void markAsDone(){
+        this.isDone = true;
+        System.out.println("\t_________________________________");
+        System.out.println("\t Nice! I've marked this task as done:");
+        System.out.printf("\t   [%s] %s\n", this.getStatusIcon(), this.description);
+        System.out.println("\t_________________________________");
+
+    }
 }
